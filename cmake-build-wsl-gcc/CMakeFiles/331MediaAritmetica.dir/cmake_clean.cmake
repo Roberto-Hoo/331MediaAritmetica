7a331b30decf3472a23aa64065703cc7f17b64c5
Roberto Hoo <roberto.hoo@ufrgs.br>
2021-04-23 16:17:43 -0300

    Initial commit

---
+++ cmake-build-wsl-gcc/CMakeFiles/331MediaAritmetica.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "331MediaAritmetica"
+  "331MediaAritmetica.pdb"
+  "CMakeFiles/331MediaAritmetica.dir/E331Media.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/331MediaAritmetica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
